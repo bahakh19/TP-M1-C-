48aa42507ec08d307ab56c684a05feb6e36d48c0
bahakh19 <123711762+bahakh19@users.noreply.github.com>
2023-02-12 00:26:28 +0100

    Add files via upload

--- exo3.cpp
+++ exo3.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-const int NMOIS = 12;
+int main()
+{
+	int n;
+	cout<<"Veuillez indiquez la hauteur de l'arbre : ";
+	cin>>n;
+	string mult = "*";
 
-struct enreg{
-    int stock;
-    float prix;
-    int ventes[NMOIS];
-};
-
-void Change(struct enreg *R){
-    (R->stock) = 0;
-    for(int i = 0; i<NMOIS; i++){
-        (R->ventes)[i] = 0;
-    }
-}
-
-int main(){
-    enreg Reg;
-    Reg.stock = 40;
-    Reg.prix = 12;
-    for(int i=0; i<NMOIS; i++){
-        Reg.ventes[i] = i;
-    }
-    enreg *p = &Reg;
-    Change(p);
-    cout<<"Stock : "<<Reg.stock<<endl;
-    for(int i=0; i<NMOIS; i++){
-        cout<<"Ventes "<<i<<" : "<<Reg.ventes[i]<<endl;
-    }
-    return 0;
+	for(int i=0;i<n;i++)
+	{
+		string eq;
+		
+		for(int j=0; j<(n-i-1); j++)
+		{
+			eq+="=";
+		}
+		if(i>0)
+		{
+			mult+="*";
+			mult+="*";
+		}
+		cout<<eq + mult + eq<<endl;
+	}
+	return 0;
 }
